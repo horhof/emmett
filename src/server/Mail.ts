9323490c738b2a61a20ac2ec91e37b6b1cb01998
Horhof <hor@hof.hh>
2017-06-17 12:21:33 -0400

    Use mailboxes not userse to address Mails

--- src/server/Mail.ts
+++ src/server/Mail.ts
@@ -1,10 +1,14 @@
+import Mailbox from "./Mailbox";
 import User from './User';
 
 export default class Mail
 {
-  public from: User;
+  public from: Mailbox;
 
-  public to: User[];
+  public to: Mailbox[];
+
+  /** If an incoming message is just to be viewed, it's accessed via an address. */
+  public address: string;
 
   /** The time the Mail should be sent. */
   public timestamp: Date;
@@ -16,4 +20,7 @@ export default class Mail
   public sent: Date | null;
 
   public read: boolean;
+
+  /** Whether this Mail is in its final Mailbox or is up for delivery. */
+  public delivered: boolean;
 }
