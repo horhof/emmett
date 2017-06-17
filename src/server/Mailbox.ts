cc0037e158394986f4ccc4d7b3dc16b3a004828e
Horhof <hor@hof.hh>
2017-06-17 12:21:53 -0400

    Include read and write keys for mailboxes

--- src/server/Mailbox.ts
+++ src/server/Mailbox.ts
@@ -1,16 +1,52 @@
 import Mail from './Mail';
 import User from './User';
 
-export default class Box
+type MaybeError = Error | void;
+
+/**
+ * Mailboxes store messages both incoming and outgoing. The exchange eventually
+ * picks up and delivers the outgoing mails.
+ * 
+ * The reading and writing of data are controlled via keys. Incoming messages
+ * are always accepted for writing but can be read only with a read key.
+ * 
+ * Outgoing messages can be read with the read key as well, but are accepted for
+ * writing only with a write key.
+ */
+export default class Mailbox
 {
-  /** Whether or not the Box is restricted just to the owner. */
+  private inbox: Mail[];
 
-  public private: boolean;
-  /** The User authorized to make changes to this Box. */
-  public owner: User;
+  /** The mailbox can be read only by providing this key, if present. */
+  private readKey: string | void;
 
-  public password: string;
+  private outbox: Mail[];
 
-  /** List of Mails in this box. */
-  public list: Mail[];
+  /** The outbox can be written to only by providing this key. */
+  private writeKey: string;
+
+  constructor(keys: string[])
+  {
+    const [readKey, writeKey] = keys;
+    this.readKey = readKey;
+    this.writeKey = writeKey;
+
+    this.inbox = [];
+    this.outbox = [];
+  }
+
+  /** Accept an incoming mail from another mailbox. */
+  public accept(mail: Mail): MaybeError
+  {
+    this.inbox.push(mail);
+  }
+
+  /** Accept a mail for delivery if the key matches. */
+  public enqueue(writeKey: string, mail: Mail): MaybeError
+  {
+    if (writeKey !== this.writeKey)
+      return new Error(`Write key did not match.`);
+
+    this.outbox.push()
+  }
 }
