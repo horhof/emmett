e020dcf4353c164814c08cfb83050f4c9e569074
Horhof <hor@hof.hh>
2017-06-17 12:22:11 -0400

    Add mailbox keys to users

--- src/server/User.ts
+++ src/server/User.ts
@@ -1,4 +1,11 @@
 export default class User
 {
+  /** For logging in. */
   public name: string;
+
+  /** For logging in. */
+  public password: string;
+
+  /** A list of secret keys for each mailbox on the exchange. */
+  public keys: { [mailbox: string]: string };
 }
