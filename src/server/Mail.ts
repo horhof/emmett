546db57a9bdf872f9c757298cb812f69b50d466e
Horhof <hor@hof.hh>
2017-06-30 23:07:43 -0400

    Clean up Mail
    - Use address for the to field
    - Add initial values for delivered / read

--- src/server/Mail.ts
+++ src/server/Mail.ts
@@ -1,26 +1,20 @@
 import Mailbox from "./Mailbox";
-import User from './User';
 
 export default class Mail
 {
   public from: Mailbox;
 
-  public to: Mailbox[];
-
-  /** If an incoming message is just to be viewed, it's accessed via an address. */
-  public address: string;
+  public to: string;
 
   /** The time the Mail should be sent. */
   public timestamp: Date;
-
-  public subject: string;
 
   public body: string;
 
   public sent: Date | null;
 
-  public read: boolean;
+  public read: boolean = false;
 
   /** Whether this Mail is in its final Mailbox or is up for delivery. */
-  public delivered: boolean;
+  public delivered: boolean = false;
 }
