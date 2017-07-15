60f32aee5632903861f581a19b75dcdbf25a9ade
Horhof <hor@hof.hh>
2017-07-15 13:25:27 -0400

    Serve static files directly, not via CORS

--- server/Server.ts
+++ server/Server.ts
@@ -1,22 +1,20 @@
 import * as Debug from 'debug';
 import * as restify from 'restify';
-const corsMiddleware = require('restify-cors-middleware');
 
 import BoxCtrl from './BoxCtrl';
 import ExchangeCtrl from './ExchangeCtrl';
 
 const log = Debug(`Emmett:Server`);
 
-const cors = corsMiddleware({
-  preflightMaxAge: 5,
-  origins: ['*']
-});
-
 const server = restify.createServer({name: 'Emmett'});
 server.use(restify.plugins.bodyParser());
-server.use(cors.preflight);
-server.use(cors.actual);
 server.listen(4096, '0.0.0.0', () => log(`Server started.`));
+
+log(`Connecting static resources...`);
+server.get(/\/*\.(html|js)/, restify.plugins.serveStatic({
+  directory: '../client',
+  default: 'index.html'
+}));
 
 const exchangeCtrl = new ExchangeCtrl(server);
 const boxCtrl = new BoxCtrl(server, exchangeCtrl.exchange);
